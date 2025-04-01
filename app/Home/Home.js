193394c42c90a38192c37c2a6edb887e84c2e599
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2025-04-01 13:44:22 +0200

    chores: r du tout

--- app/Home/Home.js
+++ app/Home/Home.js
@@ -5,7 +5,7 @@ export default function Home({ username, onLogout }) {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24, marginBottom: 20 }}>Bienvenue à la HomePage, {username}</Text>
+      <Text>Bienvenue</Text>
       <Button title="Se déconnecter" onPress={onLogout} />
     </View>
   );
