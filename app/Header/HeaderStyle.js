1c44c4bc332b5e2095c5d39d964cd1cc6340fdcf
Daphne <aviasdaphne@gmail.com>
2025-04-02 09:50:24 +0200

    séparation du style

---
+++ app/Header/HeaderStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    header: {
+      flexDirection: "row",
+      alignItems: "center",
+      backgroundColor: "#6562DF",
+      padding: 15,
+    },
+    menuText: {
+      fontSize: 30,
+      color: "white",
+    },
+    title: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    titleText: {
+      fontSize: 24,
+      color: "white",
+      fontWeight: "bold",
+    },
+  });
